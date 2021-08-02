c7fad2b07b88503f29317a27bf0cc7143d8ea7ef
Evert Arias <evert.arias@hotmail.com>
2021-08-02 10:49:41 -0400

    Update login method

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,13 +44,37 @@ const signUp = async (req, res, next) => {
 };
 
 // login
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new AppError("Please provide email and password", 400));
+    }
+
+    // Select funtion is necessary because by default password is not read from database
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError("Incorrect email or password"), 401);
+    }
+
+    const token = signToken(user._id); // FIXME: Token is not being generated here.
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production" ? true : false, // set to true when in production
+      expires: new Date(
+        Date.now() +
+          process.env.JWT_COOKIE_EXPIRATION_TIME * 24 * 60 * 60 * 1000
+      ),
+    });
+
     res.status(201).json({
       status: "sucess",
       data: {
-        // token,
-        // user: newUser,
+        token,
+        user,
       },
     });
   } catch (error) {
