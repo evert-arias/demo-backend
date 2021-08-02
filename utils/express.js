e91244c08263bbf17e29ccc0a3c2a7cbd3d93d03
Evert Arias <evert.arias@hotmail.com>
2021-08-02 10:37:05 -0400

    Add sanitation and error handling

--- utils/express.js
+++ utils/express.js
@@ -2,7 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import ExpressMongoSanitize from "express-mongo-sanitize";
 import userRouter from "../routes/userRoutes.js";
+import errorHandler from './errorHandler.js';
+
+import xss from 'xss-clean';
+import hpp from 'hpp';
 
 const app = express();
 
@@ -16,8 +21,32 @@ app.use(helmet());
 
 app.use(morgan("dev"));
 
-app.use('/api', limiter);
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+// Data sanitization
+app.use(ExpressMongoSanitize());
+app.use(xss());
+
+// Clenaing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`can´t find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(errorHandler);
 
 export { app };
