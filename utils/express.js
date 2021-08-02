a3de19716df1724c02ee0bcbf79b8e7a34de6f33
Evert Arias <evert.arias@hotmail.com>
2021-08-02 09:38:03 -0400

    Add features to express server, rate limiter, etc.

--- utils/express.js
+++ utils/express.js
@@ -1,7 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import userRouter from "../routes/userRoutes.js";
 
 const app = express();
+
+const limiter = rateLimit({
+  max: 10,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.use('/api', limiter);
 
 app.use("/api/v1/users", userRouter);
 
