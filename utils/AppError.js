df2f384b263399bf50ff048ee572db685a4bb9fe
Evert Arias <evert.arias@hotmail.com>
2021-08-02 09:07:55 -0400

    Add AppError class

---
+++ utils/AppError.js
@@ -0,0 +1,8 @@
+export default class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
